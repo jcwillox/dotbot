561f77689e267427f1b99f8f004d063857a4b422
Josh Willox <joshwillox@gmail.com>
2022-01-12 18:44:15 +1100

    Add init command

---
+++ dotbot/cmd/init.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"github.com/jcwillox/dotbot/log"
+	"github.com/jcwillox/dotbot/plugins"
+	"github.com/jcwillox/dotbot/store"
+	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var apply = false
+
+var initCmd = &cobra.Command{
+	Use:   "init <owner>[/<repo>]",
+	Short: "clone and setup a dotbot dotfiles repo",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		repo := args[0]
+		// assume repo is named dotfiles
+		if !strings.Contains(repo, "/") {
+			repo += "/dotfiles"
+		}
+		parts := strings.SplitN(repo, "/", 2)
+		name := parts[1]
+
+		err := plugins.GitConfig{
+			Url:     "https://github.com/" + repo,
+			Path:    name,
+			Name:    repo,
+			Method:  "clone_pull",
+			Shallow: false,
+		}.Run()
+		if err != nil {
+			log.Fatalln("failed to clone repo", err)
+		}
+
+		cwd, err := os.Getwd()
+		if err != nil {
+			log.Panicln("failed to get current directory", err)
+		}
+
+		store.SetSave("directory", filepath.Join(cwd, name))
+
+		if apply {
+			rootCmd.Run(rootCmd, nil)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+	initCmd.Flags().BoolVar(&apply, "apply", false, "run dotbot immediately after cloning")
+}
