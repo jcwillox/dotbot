d90dffa1b19d16e94024e0b0684d1b24f3794351
Josh Willox <joshwillox@gmail.com>
2022-03-25 23:09:36 +1100

    Allow capturing stdout/stderr from the shell directive

--- plugins/shell.go
+++ plugins/shell.go
@@ -1,15 +1,18 @@
 package plugins
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/creasty/defaults"
 	"github.com/jcwillox/dotbot/log"
+	"github.com/jcwillox/dotbot/store"
 	"github.com/jcwillox/dotbot/template"
 	"github.com/jcwillox/dotbot/utils"
 	"github.com/jcwillox/dotbot/utils/sudo"
 	"github.com/jcwillox/dotbot/yamltools"
 	"github.com/jcwillox/emerald"
 	"gopkg.in/yaml.v3"
+	"io"
 )
 
 var shellLogger = log.NewBasicLogger("SHELL")
@@ -18,6 +21,7 @@ type ShellBase []*ShellConfig
 type ShellConfig struct {
 	Desc    string
 	Command utils.Command `yaml:",inline"`
+	Capture bool
 }
 
 func (b *ShellBase) UnmarshalYAML(n *yaml.Node) error {
@@ -64,5 +68,28 @@ func (c ShellConfig) Run() error {
 	if err != nil {
 		return err
 	}
+
+	// support capturing stdout/stderr
+	if c.Capture {
+		var stdout, stderr bytes.Buffer
+		if cmd.Stdout != nil {
+			cmd.Stdout = io.MultiWriter(&stdout, cmd.Stdout)
+		} else {
+			cmd.Stdout = &stdout
+		}
+		if cmd.Stderr != nil {
+			cmd.Stderr = io.MultiWriter(&stderr, cmd.Stdout)
+		} else {
+			cmd.Stderr = &stderr
+		}
+		// add output to template variables after execution
+		defer func() {
+			store.Vars(map[string]interface{}{
+				"Stdout": stdout.String(),
+				"Stderr": stderr.String(),
+			})
+		}()
+	}
+
 	return cmd.Run()
 }
