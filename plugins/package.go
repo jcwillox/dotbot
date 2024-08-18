6e05c7ba0186996a9295a7550e9350422fceae81
Tom Gehrke <phyrog@users.noreply.github.com>
2024-08-18 06:20:52 +0200

    feat: add package plugin for homebrew (#1)
    * Implement package plugin for homebrew
    
    * add workaround for formula updates for same app version
    
    * replace removed goreleaser flag
    
    * fix goreleaser settings

--- plugins/package.go
+++ plugins/package.go
@@ -3,7 +3,10 @@ package plugins
 import (
 	"bufio"
 	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+
 	"github.com/jcwillox/dotbot/log"
 	"github.com/jcwillox/dotbot/store"
 	"github.com/jcwillox/dotbot/utils"
@@ -111,6 +114,22 @@ func (c PackageItem) InstallAll() error {
 				MaxLines: 10,
 			}
 		}
+	case "brew":
+		for _, pkg := range c.Packages {
+			version, latest := getBrewPackageVersion(pkg)
+			logPackage(pkg, version, latest)
+			if version == latest || store.DryRun {
+				break
+			}
+			command = &utils.Command{
+				Command:  "brew install -q " + pkg,
+				Shell:    false,
+				Stdout:   true,
+				Stderr:   true,
+				Sudo:     false,
+				MaxLines: 10,
+			}
+		}
 	}
 	if command == nil {
 		return nil
@@ -143,6 +162,9 @@ func logPackage(pkg string, version string, latest string) {
 
 func getOsPackager() string {
 	switch {
+	// MacOS has a binary called apt, but it's not the package manager, so brew needs to come first
+	case utils.OnPath("brew"):
+		return "brew"
 	case utils.OnPath("apt"):
 		return "apt"
 	case utils.OnPath("apk"):
@@ -233,4 +255,71 @@ func getApkPackageVersion(pkg string) (installed string, latest string) {
 		latest = installed
 	}
 	return installed, latest
+}
+
+func getBrewPackageVersion(pkg string) (installed string, latest string) {
+	cmd := execabs.Command("brew", "info", "--json=v2", pkg)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return
+	}
+	err = cmd.Start()
+	if err != nil {
+		return
+	}
+	stdoutContent, err := io.ReadAll(stdout)
+	if err != nil {
+		return
+	}
+
+	var info struct {
+		Formulae []struct {
+			Name     string `json:"full_name"`
+			Outdated bool   `json:"outdated"`
+			Versions struct {
+				Stable string `json:"stable"`
+			} `json:"versions"`
+			Installed []struct {
+				Version string `json:"version"`
+			} `json:"installed"`
+		} `json:"formulae"`
+		Casks []struct {
+			Token     string `json:"token"`
+			Version   string `json:"version"`
+			Installed string `json:"installed"`
+		} `json:"casks"`
+	}
+	err = json.Unmarshal(stdoutContent, &info)
+	if err != nil {
+		return
+	}
+
+	if len(info.Formulae) > 0 {
+		for _, f := range info.Formulae {
+			if f.Name != pkg {
+				continue
+			}
+			if len(f.Installed) < 1 {
+				return "", f.Versions.Stable
+			}
+			// Some installed versions do not match the latest versions
+			// e.g. if a new formula version was released for the same application version
+			// Just return the installed version as the latest version if the formula is not outdated
+			if !f.Outdated {
+				return f.Installed[0].Version, f.Installed[0].Version
+			}
+			return f.Installed[0].Version, f.Versions.Stable
+		}
+	}
+
+	if len(info.Casks) > 0 {
+		for _, c := range info.Casks {
+			if c.Token != pkg {
+				continue
+			}
+			return c.Installed, c.Version
+		}
+	}
+
+	return
 }
